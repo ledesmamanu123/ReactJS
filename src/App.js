37f316eb9c89e20d1db9c388c2d7c0ef96a85912
ledesmamanu <ledesmamanu345@gmail.com>
2022-11-23 22:24:02 -0300

    Primeros pasos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/title/title';
 
 function App() {
+  const lista =["Resident Evil", "God Of War", "My little pony", "FIFA 23"];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title title="Playz" lists={lista} />
       </header>
     </div>
   );
---
+++ tareas/tarea.js
@@ -0,0 +1,14 @@
+//Polyfill de FIND
+const array1 = [1,5,101];
+
+const encontrar = array1.find(elemento => elemento > 100)
+console.log(encontrar)
+
+array.prototype.customFind = function (callback){
+    let resultado
+    for (let i = 0; i < this.length; i++) {
+        let elemento = this[i]
+        
+        
+    }
+}
