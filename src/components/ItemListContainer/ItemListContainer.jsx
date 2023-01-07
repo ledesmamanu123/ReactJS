cb261c719a79d3f18f55213b27a2acb73a34b0f0
ledesmamanu <ledesmamanu345@gmail.com>
2023-01-06 22:14:52 -0300

    Error useEffect pt2

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -4,32 +4,34 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import './ItemListContainer.css'
 const ItemListContainer = ({products}) => {
- let filtroProductos = []
   //Guardamos los parametros que nos llegan en params
     let params = useParams();
     console.log(params.categoryid)
   //Declaramos un useState para guardar el estado del producto
-    const [productos, setProductos] = useState(products)
+    const [productos, setProductos] = useState([])
 
-if (params.categoryid =! undefined) {
-  let filtroProductos = products.filter((producto)=>{
-    return parseInt(params.categoryid) === producto.category;})
-    //setProductos(filtroProductos)
-      useEffect(()=>{
-      setProductos(filtroProductos);
-      },[params.categoryid])
-} else {
-  const promiseProducts = new Promise((resolve, reject) => {
-    setTimeout(()=>{
-      resolve(products)
-    },2000)
-  })
-  promiseProducts.then((response)=>{
-    console.log("atrodne")
-    setProductos(response);
-  })
-}
-
+useEffect(()=>{
+  if (params.categoryid =! undefined) {
+    let filtroProductos = products.filter((producto)=>{
+      return parseInt (params.categoryid) === producto.category;
+    })
+      setProductos(filtroProductos)
+      console.log(filtroProductos)
+      console.log("verdaddeor")
+  } else {
+    const promiseProducts = new Promise((resolve, reject) => {
+      setTimeout(()=>{
+        resolve(products)
+        console.log("adentro")
+      },2000)
+    })
+    promiseProducts.then((response)=>{
+      console.log("atrodne")
+      setProductos(response);
+    })
+  }
+  },[])
+  console.log(productos)
   return (
     <section className='sectionCards'>
     <ItemList productos={productos}/>
